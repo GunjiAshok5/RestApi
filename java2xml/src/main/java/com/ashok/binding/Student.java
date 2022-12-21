9e054d8ab4c9c08c01b981317e631d67c74e1352
Gunji Ashok <ashokgunji2017@gmail.com>
2022-12-21 20:09:54 +0530

    @XmlElement annotation added

--- java2xml/src/main/java/com/ashok/binding/Student.java
+++ java2xml/src/main/java/com/ashok/binding/Student.java
@@ -1,14 +1,23 @@
 package com.ashok.binding;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
 import lombok.Data;
 
 @Data
-@XmlRootElement
+@XmlRootElement(name="student")
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Student {
 
+	@XmlElement(name="student-id")
 	private Integer id;
+	
+	@XmlElement(name="student-name")
 	private String name;
+	
+	@XmlElement(name="student-rank")
 	private Integer rank;
 }
